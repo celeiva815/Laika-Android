ef4aa9d60a30b49a82975184e4945b6f21a4f692
celeiva815 <celeivasierra@gmail.com>
2015-08-27 20:59:20 -0300

    Agregados los 3 iconos en el adapter de ficha médica

--- app/src/main/java/social/laika/app/models/Dog.java
+++ app/src/main/java/social/laika/app/models/Dog.java
@@ -214,7 +214,7 @@ public class Dog extends Model {
             jsonObject.put(COLUMN_PERSONALITY, this.mPersonality);
             jsonObject.put(COLUMN_IS_STERILIZED, this.mIsSterilized);
             jsonObject.put(COLUMN_IS_TRAINED, this.mIsTrained);
-            jsonObject.put(COLUMN_CHIP_CODE, "");
+            jsonObject.put(COLUMN_CHIP_CODE, Do.isNullOrEmpty(this.mChipCode) ? "" : this.mChipCode);
             jsonObject.put(COLUMN_STATUS, this.mStatus);
 
         } catch (JSONException e) {
